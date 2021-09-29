449844eaf073f019ccc27ec36366fcfaffb72ea6
renatocortez <recortez@gmail.com>
2021-09-29 18:38:11 -0300

    Trabalhando com Dados em JS

--- ex004/script.js
+++ ex004/script.js
@@ -1,5 +1,5 @@
 function inicio() {
     let nome = window.prompt('Qual é seu nome?')
     let resp = window.document.getElementById('resultado')
-    resp.innerHTML = `<p>Olá, <bold>${nome}</bold>! É um grande prazer te conhecer!`
+    resp.innerHTML = `<p>Olá, <bold>${nome}</bold>! É um grande prazer te conhecer!</p>`
 }
