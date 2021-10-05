ce2dd16250c4d25b9806c8924b77048a925b630e
renatocortez <recortez@gmail.com>
2021-10-05 16:50:17 -0300

    Desafio 003 - Antes e depois

---
+++ d003/script.js
@@ -0,0 +1,8 @@
+function resultado() {
+    let n1 = Number(window.prompt('Digite um número inteiro: '))
+    mais = n1 ++
+    menos = n1 --
+
+    window.alert(`Antes de ${n1}, temos o número ${n1 - 1}. Depois de  ${n1}, temos o número ${n1 + 1}`)
+
+}
